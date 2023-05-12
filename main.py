707c1ae882acb7356ef8a4ebbdd8dc09cce2d423
Mask-K <klimenkom14@gmail.com>
2023-05-12 13:34:28 +0300

    ready to pass

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 import sweepline
 
 
+
 def generate_random_segments():
     n = int(input("Enter amount of segments to be generated(more than 50 is not recommended) => "))
     min_coord, max_coord = map(float, input("Enter bounds to be used for generation for example -5 5\n").split())
@@ -28,25 +29,20 @@ def input_segments():
 
 
 def plot_segments_and_intersections(segments, intersections):
-    # Extract x and y coordinates from line segments
     x_coords = [[segment[0][0], segment[1][0]] for segment in segments]
     y_coords = [[segment[0][1], segment[1][1]] for segment in segments]
 
-    # Plot line segments
     for x, y in zip(x_coords, y_coords):
         plt.plot(x, y, 'b')
 
-    # Plot intersection points
     intersection_x = [intersection[0] for intersection in intersections]
     intersection_y = [intersection[1] for intersection in intersections]
     plt.plot(intersection_x, intersection_y, 'ro')
 
-    # Add labels and title
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.title('Line Segments and Intersection Points')
 
-    # Display the plot
     plt.show()
 
 
